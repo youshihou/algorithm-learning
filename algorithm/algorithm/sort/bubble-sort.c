46aee05f833441a9ad1a764a19d538d8b1b6d844
oneday <youshihou5@gmail.com>
2020-05-08 22:28:42 +0800

    bubble-sort

--- algorithm/algorithm/sort/bubble-sort.c
+++ algorithm/algorithm/sort/bubble-sort.c
@@ -22,6 +22,27 @@ int* bubble_sort(int* nums, int numSize) {
     memset(list, 0, sizeof(int));
     
     for (int end = numSize - 1; end > 0; end--) {
+        int sortedIndex = 1;
+        for (int begin = 1; begin <= end; begin++) {
+            if (nums[begin] < nums[begin - 1]) {
+                swap(&nums[begin], &nums[begin - 1]);
+                sortedIndex = begin;
+            }
+        }
+        end = sortedIndex;
+    }
+
+    return list;
+}
+
+
+
+int* bubble_sort2(int* nums, int numSize) {
+    if (nums == NULL || numSize <= 0) { return nums; }
+    int* list = malloc(sizeof(int) * numSize);
+    memset(list, 0, sizeof(int));
+    
+    for (int end = numSize - 1; end > 0; end--) {
         bool sorted = true;
         for (int begin = 1; begin <= end; begin++) {
             if (nums[begin] < nums[begin - 1]) {
