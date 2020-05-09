961fe7535d07d67e088f40173124d67911ba94e7
oneday <youshihou5@gmail.com>
2020-05-09 23:26:04 +0800

    add insertion-sort

---
+++ algorithm/algorithm/sort/insertion-sort.c
@@ -0,0 +1,9 @@
+//
+//  insertion-sort.c
+//  algorithm
+//
+//  Created by Ankui on 5/9/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "insertion-sort.h"
---
+++ algorithm/algorithm/sort/insertion-sort.h
@@ -0,0 +1,14 @@
+//
+//  insertion-sort.h
+//  algorithm
+//
+//  Created by Ankui on 5/9/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef insertion_sort_h
+#define insertion_sort_h
+
+#include <stdio.h>
+
+#endif /* insertion_sort_h */
