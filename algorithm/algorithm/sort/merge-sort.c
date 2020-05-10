d9fccaae12f442bf7d745883c0c8510ab3ecb185
oneday <youshihou5@gmail.com>
2020-05-10 16:32:48 +0800

    add merge-sort

---
+++ algorithm/algorithm/sort/merge-sort.c
@@ -0,0 +1,9 @@
+//
+//  merge-sort.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "merge-sort.h"
---
+++ algorithm/algorithm/sort/merge-sort.h
@@ -0,0 +1,14 @@
+//
+//  merge-sort.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef merge_sort_h
+#define merge_sort_h
+
+#include <stdio.h>
+
+#endif /* merge_sort_h */
