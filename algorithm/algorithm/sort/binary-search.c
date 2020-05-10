ce015b90102e4e5efe6a92b37ac444a75e81f0b4
oneday <youshihou5@gmail.com>
2020-05-10 13:35:48 +0800

    add insertion-sort

---
+++ algorithm/algorithm/sort/binary-search.c
@@ -0,0 +1,9 @@
+//
+//  binary-search.c
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "binary-search.h"
---
+++ algorithm/algorithm/sort/binary-search.h
@@ -0,0 +1,14 @@
+//
+//  binary-search.h
+//  algorithm
+//
+//  Created by Ankui on 5/10/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef binary_search_h
+#define binary_search_h
+
+#include <stdio.h>
+
+#endif /* binary_search_h */
