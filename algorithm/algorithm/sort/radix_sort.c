c1b6ff496622e0939fc4e7111bffc2ee66e0013d
oneday <youshihou5@gmail.com>
2020-05-16 21:46:56 +0800

    add radix_sort

--- algorithm/algorithm/sort/radix_sort.c
+++ algorithm/algorithm/sort/radix_sort.c
@@ -9,7 +9,40 @@
 #include "radix_sort.h"
 #include "sort-common.h"
 
+void counting_sort_(int nums[], int size, int divider) {
+    int counts[10] = {0};
+    
+    for (int i = 0; i < size; i++) {
+        int idx = nums[i] / divider % 10;
+        counts[idx]++;
+    }
+    
+    for (int i = 1; i < 10; i++) {
+        counts[i] += counts[i - 1];
+    }
+    
+    int list[size];
+    for (int i = size - 1; i >= 0; i--) {
+        int idx = nums[i] / divider % 10;
+        list[--counts[idx]] = nums[i];
+    }
+    
+    for (int i = 0; i < size; i++) {
+        nums[i] = list[i];
+    }
+}
 
 void radix_sort(int nums[], int size) {
+    if (nums == NULL || size <= 0) { return; }
     
+    int max = nums[0];
+    for (int i = 0; i < size; i++) {
+        if (nums[i] > max) {
+            max = nums[i];
+        }
+    }
+    
+    for (int divider = 1; divider <= max; divider *= 10) {
+        counting_sort_(nums, size, divider);
+    }
 }
