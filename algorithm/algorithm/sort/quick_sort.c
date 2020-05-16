eab56eed05e33fa3f7b12af7bfbd851f0ec7edaf
oneday <youshihou5@gmail.com>
2020-05-16 16:44:26 +0800

    add quick_sort

---
+++ algorithm/algorithm/sort/quick_sort.c
@@ -0,0 +1,9 @@
+//
+//  quick_sort.c
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "quick_sort.h"
---
+++ algorithm/algorithm/sort/quick_sort.h
@@ -0,0 +1,14 @@
+//
+//  quick_sort.h
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef quick_sort_h
+#define quick_sort_h
+
+#include <stdio.h>
+
+#endif /* quick_sort_h */
