63917e43f15115a8ff9e305af52980fcd51ea625
oneday <youshihou5@gmail.com>
2020-05-16 22:12:30 +0800

    add bucket_sort

---
+++ algorithm/algorithm/sort/bucket_sort.c
@@ -0,0 +1,9 @@
+//
+//  bucket_sort.c
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "bucket_sort.h"
---
+++ algorithm/algorithm/sort/bucket_sort.h
@@ -0,0 +1,14 @@
+//
+//  bucket_sort.h
+//  algorithm
+//
+//  Created by Ankui on 5/16/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef bucket_sort_h
+#define bucket_sort_h
+
+#include <stdio.h>
+
+#endif /* bucket_sort_h */
