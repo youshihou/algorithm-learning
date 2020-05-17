c070a388a55709300af6e63497db172293112c19
oneday <youshihou5@gmail.com>
2020-05-17 13:11:19 +0800

    add quick_union_rank_path_spliting

---
+++ algorithm/algorithm/union-find/quick_union_rank_path_spliting.h
@@ -0,0 +1,14 @@
+//
+//  quick_union_rank_path_spliting.h
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef quick_union_rank_path_spliting_h
+#define quick_union_rank_path_spliting_h
+
+#include <stdio.h>
+
+#endif /* quick_union_rank_path_spliting_h */
