bf2cc4fe792f6d6716da20ab5a1d5503ba90ee2a
oneday <youshihou5@gmail.com>
2020-05-17 11:48:49 +0800

    add quick_union_size

---
+++ algorithm/algorithm/union-find/quick_union_rank.c
@@ -0,0 +1,9 @@
+//
+//  quick_union_rank.c
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "quick_union_rank.h"
---
+++ algorithm/algorithm/union-find/quick_union_rank.h
@@ -0,0 +1,14 @@
+//
+//  quick_union_rank.h
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef quick_union_rank_h
+#define quick_union_rank_h
+
+#include <stdio.h>
+
+#endif /* quick_union_rank_h */
