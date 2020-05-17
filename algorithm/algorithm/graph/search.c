066f81be2cdd4fd990a2f7af7d5f80b868517123
oneday <youshihou5@gmail.com>
2020-05-17 23:12:49 +0800

    add search

---
+++ algorithm/algorithm/graph/search.c
@@ -0,0 +1,9 @@
+//
+//  search.c
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "search.h"
---
+++ algorithm/algorithm/graph/search.h
@@ -0,0 +1,14 @@
+//
+//  search.h
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef search_h
+#define search_h
+
+#include <stdio.h>
+
+#endif /* search_h */
