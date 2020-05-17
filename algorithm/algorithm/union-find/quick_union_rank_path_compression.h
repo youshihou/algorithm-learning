93471ae6e28a3ec5b9415f769c12e4295f6038ad
oneday <youshihou5@gmail.com>
2020-05-17 12:59:53 +0800

    add quick_union_rank_path_compression

---
+++ algorithm/algorithm/union-find/quick_union_rank_path_compression.h
@@ -0,0 +1,14 @@
+//
+//  quick_union_rank_path_compression.h
+//  algorithm
+//
+//  Created by Ankui on 5/17/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef quick_union_rank_path_compression_h
+#define quick_union_rank_path_compression_h
+
+#include <stdio.h>
+
+#endif /* quick_union_rank_path_compression_h */
