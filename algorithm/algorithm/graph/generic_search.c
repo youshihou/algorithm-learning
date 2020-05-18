0424fad106c141b83a1d0c7ccd6d8662348576fb
oneday <youshihou5@gmail.com>
2020-05-18 22:12:46 +0800

    add generic_search

---
+++ algorithm/algorithm/graph/generic_search.c
@@ -0,0 +1,9 @@
+//
+//  generic_search.c
+//  algorithm
+//
+//  Created by Ankui on 5/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "generic_search.h"
---
+++ algorithm/algorithm/graph/generic_search.h
@@ -0,0 +1,14 @@
+//
+//  generic_search.h
+//  algorithm
+//
+//  Created by Ankui on 5/18/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef generic_search_h
+#define generic_search_h
+
+#include <stdio.h>
+
+#endif /* generic_search_h */
