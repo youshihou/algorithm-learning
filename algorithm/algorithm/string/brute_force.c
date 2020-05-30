93a68762564311b194a75d2be9ac619858531826
oneday <youshihou5@gmail.com>
2020-05-30 12:17:19 +0800

    add brute_force

---
+++ algorithm/algorithm/string/brute_force.c
@@ -0,0 +1,9 @@
+//
+//  brute_force.c
+//  algorithm
+//
+//  Created by Ankui on 5/30/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "brute_force.h"
---
+++ algorithm/algorithm/string/brute_force.h
@@ -0,0 +1,14 @@
+//
+//  brute_force.h
+//  algorithm
+//
+//  Created by Ankui on 5/30/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef brute_force_h
+#define brute_force_h
+
+#include <stdio.h>
+
+#endif /* brute_force_h */
