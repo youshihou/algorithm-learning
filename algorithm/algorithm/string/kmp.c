c5e14c0814f20dbca8cf67b9f59955034fcef42a
oneday <youshihou5@gmail.com>
2020-05-30 16:02:15 +0800

    add kmp

---
+++ algorithm/algorithm/string/kmp.c
@@ -0,0 +1,9 @@
+//
+//  kmp.c
+//  algorithm
+//
+//  Created by Ankui on 5/30/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#include "kmp.h"
---
+++ algorithm/algorithm/string/kmp.h
@@ -0,0 +1,14 @@
+//
+//  kmp.h
+//  algorithm
+//
+//  Created by Ankui on 5/30/20.
+//  Copyright © 2020 Ankui. All rights reserved.
+//
+
+#ifndef kmp_h
+#define kmp_h
+
+#include <stdio.h>
+
+#endif /* kmp_h */
